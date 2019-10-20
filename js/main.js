99843378b0e8d5e0e9017d5bed11a1c7ea32168a
pradeep.salunke@yahoo.com <pradeep.salunke@yahoo.com>
2019-10-20 19:37:44 -0400

    Final push

--- js/main.js
+++ js/main.js
@@ -6,9 +6,42 @@
  * Sample code
  */
 document.addEventListener('DOMContentLoaded', function() {
-  var ulElement = document.getElementById('links');
-  var liElement = document.createElement('li');
-  liElement.appendChild(document.createTextNode('created by main.js'));
-
-  ulElement.appendChild(liElement);
+ function toggleTable1()
+    {
+    var lTable = document.getElementById("Table1");
+    lTable.style.display = (lTable.style.display == "table") ? "none" : "table";
+	  document.getElementById("Table2").style.display="none";
+	  document.getElementById("Table3").style.display="none";
+	  document.getElementById("Table4").style.display="none";
+   }
+   
+   function toggleTable2()
+    {
+    var lTable = document.getElementById("Table2");
+    lTable.style.display = (lTable.style.display == "table") ? "none" : "table";
+		  document.getElementById("Table1").style.display="none";
+	  document.getElementById("Table3").style.display="none";
+	  document.getElementById("Table4").style.display="none";
+   }
+   
+   function toggleTable3()
+    {
+    var lTable = document.getElementById("Table3");
+    lTable.style.display = (lTable.style.display == "table") ? "none" : "table";
+		  document.getElementById("Table2").style.display="none";
+	  document.getElementById("Table1").style.display="none";
+	  document.getElementById("Table4").style.display="none";
+   }
+   
+   function toggleTable4()
+    {
+    var lTable = document.getElementById("Table4");
+    lTable.style.display = (lTable.style.display == "table") ? "none" : "table";
+		  document.getElementById("Table2").style.display="none";
+	  document.getElementById("Table3").style.display="none";
+	  document.getElementById("Table1").style.display="none";
+   }
 });
+
+
+
